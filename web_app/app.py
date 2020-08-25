709159005f979ad786c822d10e3c7f6ff1b0e196
Samuel Tan <41882756+samueltan97@users.noreply.github.com>
2020-08-25 07:25:17 -0400

    Update app.py

--- web_app/app.py
+++ web_app/app.py
@@ -11,11 +11,6 @@ import pandas as pd
 import json
 from .twilio_message import send_message
 
-import .end2end.CRAFT
-from .end2end.CRAFT import imgproc
-from .end2end.end2end import text_main_engine
-
-import .end2end.CRAFT
 from .end2end.CRAFT import imgproc
 from .end2end.end2end import text_main_engine
 
